8bcf6afc693a315eea6e3a3ba3bcb6ca92dc7a7f
mwcz <mwc@clayto.com>
2019-08-28 11:34:45 -0400

    add pfe-icon

--- themes/terminal/source/js/main.js
+++ themes/terminal/source/js/main.js
@@ -1,1 +1,28 @@
-// Add your script here
+import PfeIcon from "@patternfly/pfe-icon";
+
+PfeIcon.addIconSet(
+  "far",
+  "/icons/font-awesome/regular",
+  (iconName, setName, path) => {
+    const name = iconName.replace("far-", "");
+    return `${path}/${name}.svg`;
+  }
+);
+
+PfeIcon.addIconSet(
+  "fas",
+  "/icons/font-awesome/solid",
+  (iconName, setName, path) => {
+    const name = iconName.replace("fas-", "");
+    return `${path}/${name}.svg`;
+  }
+);
+
+PfeIcon.addIconSet(
+  "fab",
+  "/icons/font-awesome/brands",
+  (iconName, setName, path) => {
+    const name = iconName.replace("fab-", "");
+    return `${path}/${name}.svg`;
+  }
+);
